4447b3b1c367d720bf366723111df8ff53d03a14
Test <algoolm421@gmail.com>
2023-09-28 13:52:23 -0700

    init

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import './app.css'
 function App() {
   return (
     <div className="App">
